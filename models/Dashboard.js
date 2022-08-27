461ddd23ea282298cd77f4d77d5fab76153698fa
shubhra8 <57454930+shubhra8@users.noreply.github.com>
2022-08-27 17:29:01 -0500

    Added default value for date

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -21,7 +21,7 @@ Dashboard.init(
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
-      
+      defaultValue: DataTypes.NOW,
     },
      user_id: {
       type: DataTypes.INTEGER,
